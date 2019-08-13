194f06449bbfb169ae705546d0ae46157471067f
Bian Jin chen <kenjc.bian@rock-chips.com>
2019-08-13 21:12:38 +0800

    buildspec.mk: remove default value to prevent lunching failed.
    Change-Id: I6a149affbc1ab64a52ee9c404d7a0945b95e4ec1
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- buildspec.mk
+++ buildspec.mk
@@ -26,9 +26,6 @@ TARGET_BOARD_PLATFORM_GPU ?= mali-tDVx
 
 # Choose a product to build for.  Look in the products directory for ones
 # that work.
-ifndef TARGET_PRODUCT
-TARGET_PRODUCT:=rk3326
-endif
 ifndef TARGET_DEVICE
 TARGET_DEVICE:=rk3326
 endif
