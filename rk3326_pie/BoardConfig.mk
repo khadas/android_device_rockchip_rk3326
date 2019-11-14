8c2d757fd3be0e13a3c9f4fffe33240236b45dcf
Dayao Ji <jdy@rock-chips.com>
2019-11-14 11:13:30 +0800

    rk3326_pie: support A/B, disable by default
    Change-Id: I1fe2b3db7aea1043e6171cdf9430365df5ca3b30
    Signed-off-by: Dayao Ji <jdy@rock-chips.com>

--- rk3326_pie/BoardConfig.mk
+++ rk3326_pie/BoardConfig.mk
@@ -23,12 +23,35 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
-# Android Q use odm instead of oem, but for upgrading to Q, partation list cant be changed, odm will mount at /dev/block/by-name/oem
-BOARD_ODMIMAGE_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py device/rockchip/rk3326/rk3326_pie/parameter.txt oem)
+# AB image definition
+BOARD_USES_AB_IMAGE := false
+
+ifneq ($(strip $(BOARD_USES_AB_IMAGE)), true)
+    # Android Q use odm instead of oem, but for upgrading to Q, partation list cant be changed, odm will mount at /dev/block/by-name/oem
+    BOARD_ODMIMAGE_PARTITION_SIZE := $(shell python device/rockchip/common/get_partition_size.py device/rockchip/rk3326/rk3326_pie/parameter.txt oem)
+endif
 
 # No need to place dtb into boot.img for the device upgrading to Q.
 BOARD_INCLUDE_DTB_IN_BOOTIMG :=
 BOARD_PREBUILT_DTBIMAGE_DIR :=
 
-#Need to build system as root for the device upgrading to Q.
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+ifneq ($(strip $(BOARD_USES_AB_LEGACY_RETROFIT)), true)
+    # Need to build system as root for the device upgrading to Q.
+    BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+endif
+
+ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
+    ifeq ($(strip $(BOARD_USES_AB_LEGACY_RETROFIT)), true)
+        PRODUCT_FSTAB_TEMPLATE := device/rockchip/rk3326/rk3326_pie/fstab_ab_retrofit.in
+        PRODUCT_DTBO_TEMPLATE := device/rockchip/rk3326/rk3326_pie/dt-overlay_ab_retrofit.in
+    else
+        PRODUCT_FSTAB_TEMPLATE := device/rockchip/rk3326/rk3326_pie/fstab_ab.in
+        PRODUCT_DTBO_TEMPLATE := device/rockchip/rk3326/rk3326_pie/dt-overlay_ab.in
+    endif
+    include device/rockchip/common/BoardConfig_AB.mk
+    ifeq ($(strip $(BOARD_USES_AB_LEGACY_RETROFIT)), true)
+        TARGET_RECOVERY_FSTAB := device/rockchip/rk3326/rk3326_pie/recovery.fstab_AB_retrofit
+    else
+        TARGET_RECOVERY_FSTAB := device/rockchip/rk3326/rk3326_pie/recovery.fstab_AB
+    endif
+endif
